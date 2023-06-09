2bff9562b3bf82a983faa1a6114ce64a2b39d8d8
dyharlan <dyharlan45@gmail.com>
2023-06-09 10:16:14 +0800

    Add SChip 1.1 as a new machine type

--- src/Backend/MachineType.java
+++ src/Backend/MachineType.java
@@ -17,9 +17,11 @@ public enum MachineType {
 //     clipQuirks = true;
 //     vBlankQuirks = true;
 //     IOverflowQuirks = true;
+//     jumpQuirks = false;
 
-    COSMAC_VIP("COSMAC VIP",64,32,new boolean[]{false,false,true,false,true,true,true});
-    
+    COSMAC_VIP("COSMAC VIP",64,32,new boolean[]{false,false,true,false,true,true,true,false}),
+    SUPERCHIP_1_1("Super-Chip 1.1",64,32,new boolean[]{false,true,false,true,true,false,true,true});
+
     private String machineName;
     private int displayWidth;
     private int displayHeight;
